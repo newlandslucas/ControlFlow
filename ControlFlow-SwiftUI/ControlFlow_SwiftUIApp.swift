1b660f7eb882259604d563ee164fd2c6658123da
newlandslucas <lucasnewlands.m@gmail.com>
2022-06-01 21:14:25 -0300

    Initial Commit

---
+++ ControlFlow-SwiftUI/ControlFlow_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlFlow_SwiftUIApp.swift
+//  ControlFlow-SwiftUI
+//
+//  Created by Lucas Machado (Estudos) on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ControlFlow_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
